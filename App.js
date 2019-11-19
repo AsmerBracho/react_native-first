ed2accc6c360d024029b01acf410bba870855771
Asmer Bracho <asmer.bracho@Openet.com>
2019-11-19 14:56:12 +0000

    deleting items in list

--- App.js
+++ App.js
@@ -12,6 +12,12 @@ export default function App() {
     setCourseGoals(currentGoals => [...currentGoals, {id: Math.random().toString(), value: goaltitle}]);
   };
 
+const removeGoalhandler = goalId => {
+  setCourseGoals(currentGoals=> {
+    return currentGoals.filter((goal) => goal.id !== goalId);
+  });
+}
+
   return (
     <View style={styles.screen}>
       
@@ -19,7 +25,7 @@ export default function App() {
       <FlatList 
       keyExtractor= {(item, index) => item.id}
       data={courseGoals}
-      renderItem={itemData => <GoalItem onDelete={() => console.log('It is working')} title={itemData.item.value} />} 
+      renderItem={itemData => <GoalItem id={itemData.item.id} onDelete={removeGoalhandler} title={itemData.item.value} />} 
       />
     </View>
   );
